3b973a4c46f017a5a169ab5426c6c7aac749db0a
elaagaiba abdelaadim <elaagaiba33kech@gmail.com>
2023-01-24 00:59:37 +0000

    update btn-ok & told us bravo whene we click on the button ok and the number generated equal the number we put it

--- app/src/main/java/com/example/jeu_de_devinette/MainActivity.java
+++ app/src/main/java/com/example/jeu_de_devinette/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.jeu_de_devinette;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    EditText et_deviner;
+    Button btn_ok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et_deviner = findViewById(R.id.et_deviner);
+        btn_ok = findViewById(R.id.btn_Ok);
+        int max=14,min=10;
+        Random rand = new Random();
+        int value = rand.nextInt((max - min) + 1) + min;
+
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int  n1 = Integer.parseInt(et_deviner.getText().toString()),
+                n2=value;
+
+                if (n1==n2){
+                    Toast.makeText(MainActivity.this, "bravo", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "non", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
     }
 }
